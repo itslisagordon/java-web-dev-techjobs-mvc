dd53676f99b7f7032304e2eb2c992f8a0a035442
Lisa Gordon <lg.official94@gmail.com>
2020-10-16 02:13:10 -0500

    finished adding list functionality

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/ListController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/ListController.java
@@ -42,6 +42,7 @@ public class ListController {
         model.addAttribute("locations", JobData.getAllLocations());
         model.addAttribute("positions", JobData.getAllPositionTypes());
         model.addAttribute("skills", JobData.getAllCoreCompetency());
+        model.addAttribute("all", JobData.findAll());
 
         return "list";
     }
--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -25,4 +25,19 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+
+        ArrayList<Job> jobs;
+        if (searchTerm.trim().toLowerCase().equals("all") || searchTerm.trim().equals("")){
+            jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+        } else {
+            jobs = JobData.findByColumnAndValue(searchTerm, searchType);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchTerm) + ": " + searchType);
+        }
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+    }
 }
